2ab530d51617895563796605d3ec9b60e5754ea2
Flow <datvu.it@gmail.com>
2023-07-09 13:11:40 +0700

    feat: fix compatibility for flutter 3.0.5

--- lib/keyboard_actions.dart
+++ lib/keyboard_actions.dart
@@ -319,7 +319,7 @@ class KeyboardActionstate extends State<KeyboardActions>
   ///
   /// Position the overlay based on the current [MediaQuery] to land above the keyboard.
   void _insertOverlay() {
-    OverlayState os = Overlay.of(context);
+    OverlayState? os = Overlay.of(context);
     _inserted = true;
     _overlayEntry = OverlayEntry(builder: (context) {
       // Update and build footer, if any
@@ -374,7 +374,7 @@ class KeyboardActionstate extends State<KeyboardActions>
         ],
       );
     });
-    os.insert(_overlayEntry!);
+    os?.insert(_overlayEntry!);
   }
 
   /// Remove the overlay bar. Call when losing focus or being dismissed.
